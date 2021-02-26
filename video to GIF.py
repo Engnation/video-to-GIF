f038db6d92f9b77f625277611f77215d04043d49
Ryan Schmalenberg <engnation101@gmail.com>
2021-02-26 15:05:15 -0500

    Initial commit

---
+++ video to GIF.py
@@ -0,0 +1,11 @@
+from moviepy.editor import *
+from pygame import *
+
+clip = (VideoFileClip("2021-01-31 17-17-36.mkv")
+        .subclip((0,00.00),(0,09.00))
+        .resize(0.3) #1920 x 1080 -> 640 x 360
+        #x1, y1 indicate the top left corner
+        #.crop(x1=30, y1=330, x2=610, y2=30)) # remove left-right & top-bottom borders
+        .crop(x_center= 335, y_center=160, width=520, height=260))
+clip.preview()
+clip.write_gif("robot_grasp.gif",program='ffmpeg,fps=clip.fps)
