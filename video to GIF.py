67d064a0ad9198bb21a1b76dee6c6b19789a02f7
Ryan Schmalenberg <engnation101@gmail.com>
2021-02-26 15:32:33 -0500

    Cropped width for robot_grasp.gif

--- video to GIF.py
+++ video to GIF.py
@@ -6,7 +6,7 @@ clip = (VideoFileClip("2021-01-31 17-17-36.mkv")
         .resize(0.3) #1920 x 1080 -> 640 x 360
         #x1, y1 indicate the top left corner
         #.crop(x1=30, y1=330, x2=610, y2=30)) # remove left-right & top-bottom borders
-        .crop(x_center= 335, y_center=160, width=520, height=260))
+        .crop(x_center= 275, y_center=160, width=400, height=260))
 clip.preview()
 #clip.write_gif("robot_grasp.gif",program='ffmpeg',fps=clip.fps)
 clip.write_gif("robot_grasp.gif",program='ffmpeg',fps=15)
